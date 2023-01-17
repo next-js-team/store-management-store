afef128c5f61a1a6f4e26bf7a8481d3fbcb46e3c
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-01-17 11:53:44 +0700

    update view anggota

--- view/view_anggota.php
+++ view/view_anggota.php
@@ -6,115 +6,127 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Perpustakaan Management</title>
-	<!-- <script src="https://cdn.tailwindcss.com"></script> -->
+	<script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
 
-	<div class="w-full h-20 pt-8 pb-8">
-		<div class="grid place-content-center">
-			<h1 class="font-bold text-2xl text-gray-700 uppercase">Perpustakaan Management</h1>
-		</div>
+<div class="relative shadow bg-white">
+		<div class="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed bg-sky-500" style=""></div>
 
-	</div>
+		<div class="w-full backdrop-blur-sm">
+			<div class="relative z-1 h-16 mx-auto px-5 max-w-7xl flex items-center justify-between text-white">
+				<a class="text-2xl hover:text-cyan-400 transition-colors" href="">Perpustakaan Management</a>
 
-
-	<div>
-		<div>
-        <a href='index.php'><button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Home</button></a>
-			<a href='index.php?p=peminjaman'><button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Peminjaman</button></a>
-			<a href='index.php?a=anggota'><button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Anggota</button></a>
-			<a href='index.php?t=penerbit'><button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Penerbit</button></a>
-			<a href='index.php?g=pengarang'><button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Pengarang</button></a>
-
-
-		</div>
-	</div>
-
-	<div class="mx-8">
-		<div class="flex flex-col">
-			<div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-				<div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-					<div class="overflow-hidden">
-						<table class="min-w-full text-center">
-							<thead class="border-b bg-sky-500">
-								<tr>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										ID Anggota
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Nama
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Username
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Password
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Jenis Kelamin
-									</th>
-                                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										No Hp
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Alamat
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-                                        Email
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Tanggal Entry
-									</th>
-                                    <th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Role
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-										Aksi
-									</th>
-									<th scope="col" class="text-sm font-medium text-white px-6 py-4">
-
-									</th>
-								</tr>
-							</thead class="border-b">
-							<tbody>
-
-								<?php while ($row = $this->model->fetch($data)) {
-									echo "
-                                        <tr class='bg-white border-b'>
-                                            <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>$row[0]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[1]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[2]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[3]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[4]</td>
-                                            <td class='px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900'>$row[5]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[6]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[7]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[8]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>$row[9]</td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'><a href='index.php?e=$row[0]'>   <button type='button' class='inline-block px-6 py-2.5 bg-yellow-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out'>edit</button></a></td>
-                                            <td class='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'><a href='index.php?d=$row[0]' onClick=\"return confirm('Hapus Data?')\"\><button type='button' class='inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out'>Delete</button></a></td>
-                                        </tr>
-                                    ";
-								} ?>
-
-
-
-
-
-							</tbody>
-						</table>
-						<div class="my-2 ">
-							<div class="flex space-x-2 justify-center">
-								<a href='index.php?i=add'><button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Tambah Data</button></a>
-
-							</div>
-						</div>
-						<center></center>
-					</div>
-				</div>
+				<ul class="flex items-center gap-5">
+					<li><a class="hover:text-cyan-400 transition-colors" href='index.php'>Home</a></li>
+					<li><a class="hover:text-cyan-400 transition-colors" href='index.php?p=peminjaman'>Peminjaman</a></li>
+					<li><a class="hover:text-cyan-400 transition-colors" href='index.php?a=anggota'>Anggota</a></li>
+					<li><a class="hover:text-cyan-400 transition-colors" href='index.php?t=penerbit'>Penerbit</a></li>
+					<li><a class="hover:text-cyan-400 transition-colors" href='index.php?g=pengarang'>Pengarang</a></li>
+				</ul>
 			</div>
 		</div>
+	</div>
+
+	<div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10 mx-6">
+		<div>
+
+			<a href='index.php?i=add'><button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Tambah</button></a>
+
+
+		</div>
+		<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 overflow-x-scroll">
+			<thead class="overflow-x-scroll text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+				<tr>
+
+					<th scope="col" class="px-6 py-3">
+						ID Anggota
+					</th>
+					<th scope="col" class="px-6 py-3">
+						Nama
+					</th>
+					<th scope="col" class="px-6 py-3">
+						username
+					</th>
+					<th scope="col" class="px-6 py-3">
+						Password
+					</th>
+					
+					<th scope="col" class="px-6 py-3">
+						Jenis Kelamin
+					</th>
+					<th scope="col" class="px-6 py-3">
+						No Telepon
+					</th>
+					<th scope="col" class="px-6 py-3">
+						Alamat
+					</th>
+					<th scope="col" class="px-6 py-3">
+						Email
+					</th>
+					<th scope="col" class="px-6 py-3">
+						Tanggal Entry
+					</th>
+					<th scope="col" class="px-6 py-3">
+						Role
+					</th>
+					
+
+					<th scope="col" class="px-6 py-3">
+						Action
+					</th>
+				</tr>
+			</thead>
+			<tbody class="overflow-y-scroll w-full" style="height: 20vh;">
+
+				<?php while ($row = $this->model->fetch($data)) {
+					echo "
+						<tr class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+
+							<th scope='row' class='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'>
+								$row[0]
+							</th>
+							<td class='px-6 py-4'>
+							$row[1]
+							</td>
+							<td class='px-6 py-4'>
+							$row[2]
+							</td>
+							<td class='px-6 py-4'>
+							$row[3]
+							</td>
+							<td class='px-6 py-4'>
+							$row[4]
+							</td>
+							<td class='px-6 py-4'>
+							$row[5]
+							</td>
+							<td class='px-6 py-4'>
+							$row[6]
+							</td>
+							<td class='px-6 py-4'>
+							$row[7]
+							</td>
+							<td class='px-6 py-4'>
+							$row[8]
+							</td>
+							<td class='px-6 py-4'>
+							$row[9]
+							</td>
+							
+							<td class='flex items-center px-6 py-4 space-x-3'>
+								<a href='index.php?e=$row[0]' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+								<a href='index.php?d=$row[0]' onClick=\"return confirm('Hapus Data?')\" class='font-medium text-red-600 dark:text-red-500 hover:underline'>Remove</a>
+							</td>
+						</tr>
+					
+					";
+				} ?>
+
+
+			</tbody>
+		</table>
 	</div>
 </body>
 
